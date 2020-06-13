33f54f60e90f37b2c4358826c75e0d41ee443675
kumudug <kumudug1@gmail.com>
2020-06-13 16:38:12 -0700

    Extract the card as a Component
    * Extract the card view as a reusable component
    * Supports overriding and adding more styles into the card component
    * Shows the components sent within the card

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
+import Card from '../components/Card';
+
 type AppProps = {
 
 };
@@ -9,14 +11,14 @@ const StartGameScreen = (props: AppProps) => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game!</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonPanel}>
                     <Button title="Reset" onPress={() => { }} />
                     <Button title="Confirm" onPress={() => { }} />
                 </View>
-            </View>
+            </Card>
         </View>
     );
 }
@@ -35,18 +37,7 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 5,
-        padding: 20,
-        borderRadius: 10
+        alignItems: 'center'
     },
     buttonPanel: {
         flexDirection: 'row',
