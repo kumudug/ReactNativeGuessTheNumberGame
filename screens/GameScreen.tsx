8fdca8f6e1016dec19000389927e1c5425afaa06
kumudug <kumudug1@gmail.com>
2020-06-16 07:55:18 -0700

    Game Screen
    * Create a skeletal game screen

---
+++ screens/GameScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+type AppProps = {
+};
+
+const GameScreen = (props: AppProps) => {};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
