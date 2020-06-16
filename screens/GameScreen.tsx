2e28726a092b4e7a88d4634fad36f433d4d461a0
kumudug <kumudug1@gmail.com>
2020-06-16 08:12:46 -0700

    Random Number Generation for Game
    * Creates a rnd number between min, max that excludes the specified
    exclude

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -4,7 +4,20 @@ import { View, Text, StyleSheet } from 'react-native';
 type AppProps = {
 };
 
-const GameScreen = (props: AppProps) => {};
+const generateRandomNumberBetween = (min: number, max: number, exclude: number): number => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+        return generateRandomNumberBetween(min, max, exclude);
+    } else {
+        return rndNum;
+    }
+};
+
+const GameScreen = (props: AppProps) => {
+
+};
 
 const styles = StyleSheet.create({});
 
