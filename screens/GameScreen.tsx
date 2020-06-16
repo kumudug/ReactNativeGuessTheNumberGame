e9d7644c176f7edf4b32cefbf767f98edc307367
kumudug <kumudug1@gmail.com>
2020-06-16 08:26:00 -0700

    Game screen build
    * Added the initial components to the game screen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
 
 type AppProps = {
+    userChoice: number
 };
 
 const generateRandomNumberBetween = (min: number, max: number, exclude: number): number => {
@@ -16,9 +20,35 @@ const generateRandomNumberBetween = (min: number, max: number, exclude: number):
 };
 
 const GameScreen = (props: AppProps) => {
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomNumberBetween(1, 100, props.userChoice)
+    );
 
+    return (
+        <View style={styles.screen}>
+            <Text>Oponent's Guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonPanel}>
+                <Button title="LOWER" onPress={() => { }} />
+                <Button title="GREATER" onPress={() => { }} />
+            </Card>
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonPanel: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
+});
 
 export default GameScreen;
