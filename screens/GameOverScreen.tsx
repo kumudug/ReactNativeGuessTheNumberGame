88654a20355f73d025db126d2396bfc445beed59
kumudug <kumudug1@gmail.com>
2020-06-19 08:50:14 -0700

    Add image to game over screen
    * Image added
    * Fitted into a rounded container

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
@@ -14,6 +14,13 @@ const GameOverScreen = (props: AppProps) => {
     return (
         <View style={styles.screen}>
             <TitleText>Game Over !</TitleText>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/success.png')}
+                    resizeMode="cover"
+                />
+            </View>
             <BodyText>Number of rounds: {props.guessCount}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title="New Game" onPress={props.onStartGame} />
@@ -26,6 +33,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 8
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
     }
 });
 
