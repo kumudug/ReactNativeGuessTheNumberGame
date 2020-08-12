aae5db34ba25a19c2994432cb2175dcef7c3d4e0
kumudug <kumudug1@gmail.com>
2020-08-12 07:57:40 -0700

    Using Platform.select
    * Use Platform.select to clean up the code that was repeating Platfor.os
    a lot

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,22 +10,37 @@ type AppProps = {
 
 const Header = (props: AppProps) => {
     return (
-        <View style={styles.header}>
+        <View style={{
+            ...styles.headerBase,
+            ...Platform.select({
+                ios: styles.headerIOS,
+                android: styles.headerAndroid
+            })
+        }}>
             <TitleText style={styles.headerTitle}>{props.title}</TitleText>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
+
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+
+    },
+    headerIOS: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary,
+        borderBottomColor: 'transparent',
+        borderBottomWidth: 0
     },
     headerTitle: {
         color: Platform.OS === 'ios' ? Colors.primary : 'white',
